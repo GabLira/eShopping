1e9e6f2482a94a5aeb933431b1a705ca3329a7db
GabLira <oficialgabiel13@gmail.com>
2022-02-24 15:52:37 -0400

    aligning the list

--- js/app.js
+++ js/app.js
@@ -1,19 +1,48 @@
 import {buildListTPL} from "./components/templates/mobile-list.js";
 import {PrinterManager} from "./components/templates/manager.js";
-import {Products} from "./components/products.js";
 import {lateralMenu} from "./components/lateral-menu.js";
 
 const execute = () => {
 //Variables
     const container = document.querySelector('.container');
+
+    const getProducts = async () => {
+        try {
+            let result = await axios('./json/database.json');
+            console.log(52, result)
+            container.innerHTML += buildListTPL(result.data);
+            console.log(22, 'container: ' + container, 'result' + result.data)
+        } catch (e) {
+            container.innerHTML = 'La API fallo'
+        }
+    }
+    addEventListener('load', getProducts);
+    /*
+    const container = document.querySelector('.container');
+    let products = [];
     const printer = new PrinterManager();
-    window.products = new Products();
+    // window.products = new Products();
 
     printer.print('.container', buildListTPL(Array.from(products.items.values())));
 
-    const build = element => {
-        const button = element.querySelector('.product__item');
+    async function loadItems() {
+        return (await axios('./json/database.json')).json()
     }
 
+    container.addEventListener('DOMContentLoaded', async () => {
+        try {
+            products = await loadItems();
+        } catch (e) {
+            console.log('Error!');
+            console.log(e);
+        }
+        console.log(22, products);
+    });
+    const build = element => {
+        const button = element.querySelector('.product__item');
+    };
+
+    const get
+*/
 }
 window.addEventListener("DOMContentLoaded", execute);
--- js/components/templates/mobile-list.js
+++ js/components/templates/mobile-list.js
@@ -1,15 +1,20 @@
 export const buildListTPL = electronics => {
-    const output = electronics.reduce((previous, item) => {
-        return `${previous}
+    try {
+        const output = electronics.reduce((previous, item) => {
+            return `${previous}
         <li class="list__items">
             <button class="list__items--btn" data-id="${item.id}">Add to cart</button>
             <img src=${item.image} alt=${item.title} title=${item.title}
              class="list__items--image">
             <header class="list__items--name">${item.name}</header>
-            <p class="list__items--price">${item.price}</p>
+            <p class="list__items--price">Price: ${item.price}</p>
             <div class="error__message"></div>
         </li>
         `
-    });
-    return `<ul>${output}<ul/>`
+        });
+        return `<ul class="container__list">${output}<ul/>`
+    }catch (e) {
+        console.log(e)
+    }
+
 };
